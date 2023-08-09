5dfe0f4350420d57f987e76694e982590167bebd
Emin Boz <eminboz@emins-air.edu.ds.fhnw.ch>
2023-08-09 18:38:07 +0200

    fix: Variable namings

--- src/server.c
+++ src/server.c
@@ -1,47 +1,48 @@
 #include "server.h"
 
 int start_server() {
-    int socket_desc , client_sock , c;
-    struct sockaddr_in server , client;
+    int server_socket , client_socket , c;
+    struct sockaddr_in server_addr , client_addr;
     char client_message[MAX];
-     
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
+    
+    server_socket = socket(AF_INET , SOCK_STREAM , 0);
+    if (server_socket == -1)
     {
         printf("Could not create socket");
     }
     puts("Socket created");
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
      
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(PORT);
-     
-    if(bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    if(bind(server_socket,(struct sockaddr *)&server_addr , sizeof(server_addr)) < 0)
     {
         perror("bind failed. Error");
         return 1;
     }
     puts("bind done");
      
-    listen(socket_desc , 3);
-     
+    listen(server_socket , 3);
+    
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
-     
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    
+    while( (client_socket = accept(server_socket, (struct sockaddr *)&client_addr, (socklen_t*)&c)) )
     {
         puts("Connection accepted");
+        printf("Socket val: %d\n", client_socket);
          
         memset(client_message, '\0', sizeof(client_message));
-        if(recv(client_sock, client_message, MAX, 0) < 0)
+        if(recv(client_socket, client_message, MAX, 0) < 0)
         {
             puts("Recv failed");
             return 1;
         }
-         
+        
         puts("Client message received. Now sending it back...");
          
-        if(send(client_sock, client_message, strlen(client_message), 0) < 0)
+        if(send(client_socket, client_message, strlen(client_message), 0) < 0)
         {
             puts("Send failed");
             return 1;
@@ -50,7 +51,7 @@ int start_server() {
         puts("Message sent back to client");
     }
      
-    if (client_sock < 0)
+    if (client_socket < 0)
     {
         perror("Accept failed");
         return 1;
