6193b2fdac9845c208f5b93f028995fb6ff7d877
Emin Boz <eminboz@Emins-MacBook-Air.local>
2023-08-11 02:39:46 +0200

    feat: Add capability to handle multiple clients by enabling multithreading

--- src/server.c
+++ src/server.c
@@ -1,11 +1,12 @@
 #include "server.h"
 
+static uint32_t chatroom_id_counter = 0;
+
 int start_server() {
-    int server_socket , client_socket , c;
+    int server_socket , c;
     struct sockaddr_in server_addr , client_addr;
-    char client_message[MAX];
     
-    server_socket = socket(AF_INET , SOCK_STREAM , 0);
+    server_socket = socket(AF_INET, SOCK_STREAM , 0);
     if (server_socket == -1)
     {
         printf("Could not create socket");
@@ -16,56 +17,82 @@ int start_server() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
      
-    if(bind(server_socket,(struct sockaddr *)&server_addr , sizeof(server_addr)) < 0)
+    if (bind(server_socket,(struct sockaddr *)&server_addr , sizeof(server_addr)) < 0)
     {
         perror("bind failed. Error");
         return 1;
     }
     puts("bind done");
-     
+    
     listen(server_socket , 3);
     
     puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
+
+    uint32_t number_of_threads = 2;
+    pthread_t *tid = calloc(number_of_threads, sizeof(pthread_t));
     
-    while( (client_socket = accept(server_socket, (struct sockaddr *)&client_addr, (socklen_t*)&c)) )
-    {
-        puts("Connection accepted");
-        printf("Socket val: %d\n", client_socket);
-        printf("Entering message loop for client socket %d...\n", client_socket);
+    client_connection_args args;
+    args.server_socket = server_socket;
+    args.socket_addr_struct_size = c;
 
-        while(1) {
-            memset(client_message, '\0', sizeof(client_message));
-            if(recv(client_socket, client_message, MAX, 0) < 0)
-            {
-                puts("Recv failed");
-                return 1;
-            }
-
-            if (strcmp(client_message, "exit\n") == 0) {
-                printf("Message loop of socket %d exited.\n", client_socket);
-                break;
-            }
-            
-            puts("Client message received. Now sending it back...");
-            
-            if(send(client_socket, client_message, strlen(client_message), 0) < 0)
-            {
-                puts("Send failed");
-                return 1;
-            }
-            
-            puts("Message sent back to client");
-        }
+    for(int i = 0; i < number_of_threads; i++) {
+        pthread_create(&tid[i], NULL, (void*) &connect_client, &args);
     }
-     
-    if (client_socket < 0)
-    {
-        perror("Accept failed");
-        return 1;
+
+    for(int i = 0; i < number_of_threads; i++) {
+        pthread_join(tid[i], NULL);
     }
 
     close(server_socket);
 
     return 0;
+}
+
+
+int connect_client(client_connection_args* args) {
+    int client_socket;
+    struct sockaddr_in client_address;
+    char client_message[MAX];
+    uint32_t chatroom_id;
+    
+    while( (client_socket = accept(args->server_socket, (struct sockaddr *)&client_address, (socklen_t*)&args->socket_addr_struct_size)) )
+    {
+        chatroom_id_counter++;
+        chatroom_id = chatroom_id_counter;
+
+        printf("Connection from client with socket address %d and descriptor %d accepted. Entering echo chat room %d...\n", client_address.sin_addr.s_addr, client_socket, chatroom_id);
+        
+        int stat = 0;
+        while(1) {
+            puts("Currently in message loop");
+            memset(client_message, '\0', sizeof(client_message));
+
+            // recv is blocking the thread until a message is receiced, so while(1) loop is safe.
+            if(recv(client_socket, client_message, MAX, 0) <= 0)
+            {
+                printf("Recv from socket %d in chat room %d failed.\n", client_socket, chatroom_id);
+                return 1;
+            }
+            
+            if (strcmp(client_message, "exit\n") == 0) {
+                printf("Client %d exited chat room %d.\n", client_socket, chatroom_id);
+                close(args->server_socket);
+                break;
+            }
+            
+            printf("Message from client %d in chat room %d received: '%s'. Now sending it back...\n", client_socket, chatroom_id, client_message);
+            
+            if(send(client_socket, client_message, strlen(client_message), 0) < 0) {
+                printf("Send from server %d in chat room %d to client socket %d failed.\n", args->server_socket, chatroom_id, client_socket);
+                close(args->server_socket);
+                return 1;
+            }
+        }
+    }
+
+    if (client_socket < 0)
+    {
+        perror("Could not accept any client connection");
+        return 1;
+    }
 }
--- src/server.h
+++ src/server.h
@@ -1,15 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include<stdio.h>
-#include<string.h> 
-#include<sys/socket.h>
-#include<arpa/inet.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+#include <sys/socket.h>
+#include <arpa/inet.h> 
 #include <unistd.h>
+#include <pthread.h>
 
+#define MAX_CLIENT_CONNS 100
 #define MAX 2000
 #define PORT 8888
 
+typedef struct {
+    int server_socket;
+    int socket_addr_struct_size;
+} client_connection_args;
+
 int start_server();
+int connect_client(client_connection_args* args);
 
 #endif
